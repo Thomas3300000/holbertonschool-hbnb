d8a647e25fc215b4261e5a401637b0a7843fe1ce
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-18 10:44:16 +0000

    Add comments in places.py

--- Model/places.py
+++ Model/places.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 from datetime import datetime
+from Model.user import User
 
 
 """ A class named Place that defines a place. """
@@ -26,90 +27,114 @@ class Place:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+    """ A method that gets the name. """
     @property
     def name(self):
         return self._name
 
+    """ A method that defines the value of the name. """
     @name.setter
     def name(self, value):
         self._name = value
 
+    """ A method that gets the description. """
     @property
     def description(self):
         return self._description
 
+    """ A method that defines the value of the description. """
     @description.setter
     def description(self, value):
         self._description = value
 
+    """ A method that gets the address. """
     @property
     def address(self):
         return self._address
 
+    """ A method that defines the value of the address. """
     @address.setter
     def address(self, value):
         self._address = value
 
+    """ A method that gets the city. """
     @property
     def city(self):
         return self._city
 
+    """ A method that defines the value of the city. """
     @city.setter
     def city(self, value):
         self._city = value
 
+    """ A method that gets the lattitude. """
     @property
     def latitude(self):
         return self._latitude
 
+    """ A method that defines the value of the lattitude. """
     @latitude.setter
     def latitude(self, value):
         self._latitude = value
 
+    """ A method that gets the longitude. """
     @property
     def longitude(self):
         return self._longitude
 
+    """ A method that defines the value of the longitude. """
     @longitude.setter
     def longitude(self, value):
         self._longitude = value
 
+    """ A method that gets the host. """
     @property
     def host(self):
         return self._host
 
+    """ A method that defines the value of the host. """
     @host.setter
     def host(self, value):
+        if not isinstance(value, User):
+            raise ValueError("Host must be a User instance")
         self._host = value
 
+    """ A method that gets the number of rooms. """
     @property
     def num_rooms(self):
         return self._num_rooms
 
+    """ A method that defines the value of the number of rooms. """
     @num_rooms.setter
     def num_rooms(self, value):
         self._num_rooms = value
 
+    """ A method that gets the number of bathrooms. """
     @property
     def num_bathrooms(self):
         return self._num_bathrooms
 
+    """ A method that defines the value of the number of bathrooms. """
     @num_bathrooms.setter
     def num_bathrooms(self, value):
         self._num_bathrooms = value
 
+    """ A method that gets the price per night. """
     @property
     def price_per_night(self):
         return self._price_per_night
 
+    """ A method that defines the value of the price per night. """
     @price_per_night.setter
     def price_per_night(self, value):
         self._price_per_night = value
 
+    """ A method that gets the max number of guests. """
     @property
     def max_guests(self):
         return self._max_guests
 
+    """ A method that defines the value of the max number of guests. """
     @max_guests.setter
     def max_guests(self, value):
         self._max_guests = value
@@ -141,7 +166,6 @@ class Place:
             self.amenities.remove(amenity)
 
     """ A method that adds new reviews. """
-
     def add_review(self, review):
         """ Adds a new review. """
         self.reviews.append(review)
@@ -161,8 +185,3 @@ class Place:
                 return review
         return None
 
-    """ A method that displays a comprehensive message for the user. """
-    def __str__(self):
-        """ Returns the name of the place and the city. """
-
-        return f"{self._name} in {self._city}"
