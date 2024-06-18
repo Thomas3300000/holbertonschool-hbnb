324b769206f70e5a4c53100831d28bbdf474218b
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-18 10:49:08 +0000

    Add the comments in amenities.py

--- Model/amenities.py
+++ Model/amenities.py
@@ -11,26 +11,28 @@ class Amenity:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+    """ A method that gets the amenity_id. """
     @property
     def amenity_id(self):
         return self._amenity_id
 
-
+    """ A method that defines the value of the amenity_id. """
     @amenity_id.setter
     def amenity_id(self, value):
         self._amenity_id = value
 
+    """ A method that gets the name. """
     @property
     def name(self):
         return self._name
 
+    """ A method that defines the value of the name. """
     @name.setter
     def name(self, value):
         self._name = value
 
+    """ A method that defines the update time. """
     def update_at(self, new_name):
         self.name = new_name
         self.updated_at = datetime.now()
 
-    def __str__(self):
-        return f"Amenity: {self.name}"
